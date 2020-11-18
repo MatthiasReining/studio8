53aa06c70da0fae9ac8073886823bddee48cfdf4
Matthias Reining <matthias.reining@tech11.com>
2020-11-18 22:23:35 +0100

    base infrastructure

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var browserSync = require("browser-sync").create();
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task("sass", function () {
+  return gulp
+    .src("scss/*.scss")
+    .pipe(sass())
+    .pipe(gulp.dest("app/css"))
+    .pipe(browserSync.stream());
+});
+
+// Static Server + watching scss/html files
+gulp.task(
+  "serve",
+  gulp.series("sass", () => {
+    //gulp.series("", function () {
+    browserSync.init({
+      server: "./app/",
+    });
+
+    gulp.watch("scss/*.scss", gulp.series("sass"));
+    gulp.watch("app/*.html").on("change", browserSync.reload);
+  })
+);
+
+gulp.task("default", gulp.series("serve"));
